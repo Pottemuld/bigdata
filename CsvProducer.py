5cfb646b384613002cbca9ff55f5b1e5baa2b05c
Rasmus Jensen <rj@blue-ocean-robotics.com>
2020-12-11 14:00:09 +0100

    base functionality of tweetimplementation

--- CsvProducer.py
+++ CsvProducer.py
@@ -16,7 +16,7 @@ class CsvProducer:
             csv_file = csv.DictReader(file, delimiter = '\t')
             for row in csv_file:
                 json_object =json.dumps(dict(row), indent=4)
-                self.producer.send("csv_test", bytearray(json_object, encoding='utf-8'))
+                self.producer.send("csv_test", bytes(json_object, encoding='utf-8'))
                 self.producer.flush()
 
 if __name__ == "__main__":
--- TwitterPlayground.py
+++ TwitterPlayground.py
@@ -3,6 +3,7 @@ import json
 from kafka import KafkaConsumer
 
 bearer_token = 'AAAAAAAAAAAAAAAAAAAAANnZKQEAAAAA1dCBWQ6qyYCmG2LBdDTvBZ%2FUHpE%3Dx6SELuPPaUUVgNsF4YGYxEgtXQkwhoUskKTyQMQ3teqXOgOxCo'
+bearer_token2 = 'AAAAAAAAAAAAAAAAAAAAAPLZKQEAAAAAu7d%2F9pJlC2fbDNjDFKDhhjH8rTc%3DoHfa0tcrR5BoQwl9Ml87Jt3DeC6N6nmtG0RfILM8dLuZCyjZTo'
 headers = {"Authorization": "Bearer " + bearer_token}
 
 consumer = KafkaConsumer(
@@ -16,13 +17,32 @@ base_request = 'https://api.twitter.com/2/tweets/'
 expansion_author = '?expansions=author_id'
 expansion_location = '?expansions=geo.place_id&place.fields=contained_within,country,country_code,full_name,geo,id,name,place_type'
 
-#for m in consumer:
-#tweet = json.loads(m.value)
-tweet_id = '1315140365503156225'
-response_tweet = requests.get(base_request + tweet_id + expansion_author + '', headers=headers).json()
-author_id = response_tweet['data']['author_id']
-response_user = requests.get('https://api.twitter.com/2/users/' + author_id + '?user.fields=id,location,name', headers=headers).json()
+for m in consumer:
+    tweet = json.loads(m.value)
+    tweet_id = tweet['tweet_id']
+    if tweet['country_code'] != 'NULL':
+        #tweet_id = '1315140365503156225'
+        response_tweet = requests.get(base_request + tweet_id + expansion_location + '', headers=headers).json()
+        
+        if not 'errors' in response_tweet:
+            print(str(response_tweet['includes']['places'][0]['country_code']))
+            #send to topic
+    
+    
+#Check for error
 
-print(response_user)
+#check for geo
 
-
+#check for user locaion
+    else:
+        response_tweet = requests.get(base_request + tweet_id + expansion_author + '', headers=headers).json()
+        if not 'errors' in response_tweet:        
+            author_id = response_tweet['data']['author_id']
+            response_user = requests.get('https://api.twitter.com/2/users/' + author_id + '?user.fields=id,location,name', headers=headers).json()
+            if 'location' in response_user['data']:
+                print(response_user['data']['location'])
+                #send to topic
+            else:
+                #Send unknown to topic 
+                pass
+            

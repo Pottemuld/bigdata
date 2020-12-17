850f378251fd242f6e3fe1d6c4c21f12ebcb2570
Rasmus Jensen <rj@blue-ocean-robotics.com>
2020-12-17 12:20:43 +0100

    test of data path

--- CsvProducer.py
+++ CsvProducer.py
@@ -9,14 +9,16 @@ class CsvProducer:
         bootstrap_servers=['10.123.252.210:9092'])
 
     def readFile(self, date):
+        #filepath = "hdfs://node-master:9000/data/dailies/" + date + "/" + date + "_clean-dataset.tsv"
         filepath = "/home/hadoop/dailies/" + date + "/" + date + "_clean-dataset.tsv"
 
         with open(filepath, 'r') as file:
             csv_file = csv.DictReader(file, delimiter = '\t')
             for row in csv_file:
                 json_object =json.dumps(dict(row), indent=4)
-                self.producer.send('tweet.dataset.source', bytes(json_object, encoding='utf-8'))
-                self.producer.flush()
+                print(json_object)
+                #self.producer.send('tweet.dataset.source', bytes(json_object, encoding='utf-8'))
+                #self.producer.flush()
 
 if __name__ == "__main__":
     producer = CsvProducer()
